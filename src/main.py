3a081612f64deea93560cbf84fe37ffaa2c8944d
Piotr Sacharuk <piotr.sacharuk@gmail.com>
2025-12-09 21:13:18 +0100

    fix: update security headers according to OWASP scan

--- src/main.py
+++ src/main.py
@@ -23,7 +23,13 @@ def add_security_headers(response):
     response.headers["X-Content-Type-Options"] = "nosniff"
     response.headers["Content-Security-Policy"] = "default-src 'self'"
     response.headers["Permissions-Policy"] = "geolocation=(), microphone=()"
-    response.headers["Server"] = "unknown"  # Obfuscate server info
+    # Spectre protection
+    response.headers["Cross-Origin-Opener-Policy"] = "same-origin"
+    response.headers["Cross-Origin-Embedder-Policy"] = "require-corp"
+    # Prevent caching
+    cache_control_header = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Cache-Control"] = cache_control_header
+    response.headers["Server"] = "unknown"
     return response
 
 
