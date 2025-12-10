c6348635fd8bd1f66705a424f47c273c7013dc19
Piotr Sacharuk <piotr.sacharuk@gmail.com>
2025-12-10 15:57:53 +0100

    test: add route to simulate error

--- src/main.py
+++ src/main.py
@@ -68,6 +68,11 @@ def metrics():
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 
+@app.route("/error")
+def error():
+    return {"error": "Internal Server Error"}, 500
+
+
 @app.after_request
 def add_security_headers(response):
     response.headers["X-Content-Type-Options"] = "nosniff"
