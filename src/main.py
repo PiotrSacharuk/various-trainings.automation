ff793fe520cbdb5b7a3d32150d843e696924ab70
Piotr Sacharuk <piotr.sacharuk@gmail.com>
2025-12-10 10:50:07 +0100

    chore: standarize Flask port to 5000

--- src/main.py
+++ src/main.py
@@ -35,6 +35,6 @@ def add_security_headers(response):
 
 if __name__ == "__main__":
     host = os.getenv("FLASK_HOST", "0.0.0.0")  # nosec B104
-    port = int(os.getenv("FLASK_PORT", "8080"))
+    port = int(os.getenv("FLASK_PORT", "5000"))
     debug = os.getenv("FLASK_DEBUG", "False").lower() == "true"
     app.run(host=host, port=port, debug=debug)
